ecbcc830f735d7184a5cea0bb5205e6e02be0e9e
Rafael Motta de Carvalho <rafaelmcarvalho@gmail.com>
2010-11-23 10:00:57 -0200

    Commit inicial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nucest::Application.config.secret_token = 'ab7b7794999825980df71a9520290039496df9d000ef1ad853c1c1dc259ef2424b69015075c46c21d16ca8e8f0cb703fd2553580da9da9c5027b6d44afbf9455'
