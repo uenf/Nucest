f4f8553b984d67d63a74b211115425d19f8f9bb3
Herond Robaina Salles <hrsalles@republica.(none)>
2011-04-20 01:37:42 -0300

    Sync.

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110330071321) do
+ActiveRecord::Schema.define(:version => 70) do
 
   create_table "areas", :force => true do |t|
     t.string   "nome"
