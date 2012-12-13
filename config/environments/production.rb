2786dd9ce70db5c5c479826df969cbb85653546c
Rodrigo Manhães <rmanhaes@gmail.com>
2012-12-13 10:35:40 -0200

    incluido manualmente jqModal.js para ser precompilado em producao

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,5 @@ Nucest::Application.configure do
   # config.assets.manifest = YOUR_PATH
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( jqModal.js )
 end
-
