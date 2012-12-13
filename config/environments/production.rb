64a7fdfcce7ccfa6eb7f11d44f3ec64fcba73fcc
Rodrigo Manhães <rmanhaes@gmail.com>
2012-12-13 11:04:07 -0200

    incluido manualmente cadastro_instituicao.js para ser precompilado em producao

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ Nucest::Application.configure do
   # config.assets.manifest = YOUR_PATH
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( jqModal.js )
+  config.assets.precompile += %w(jqModal.js cadastro_instituicao.js)
 end
